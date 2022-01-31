e143d41e3dc2b6b08640b75a6ac3562be2edf2f4
Jagad R R <43224845+ZenthicMC@users.noreply.github.com>
2022-01-31 20:21:33 +0700

    Update register.php

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>E-Perpus | Register</title>
+    <title>Register</title>
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href=".assets/fonts/fontawesome-all.min.css">
@@ -61,7 +61,7 @@
                         confirmButtonText: 'OK',
                         }).then((result) => {
                         if (result.isConfirmed) {
-                            location.href = "welcome.php";
+                            location.href = "login.php";
                         }
                         })
                     </script>
